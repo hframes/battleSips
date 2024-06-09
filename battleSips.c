503abcece1e808c96656c1300c0abd004e3ed4af
Hampus Farmängen <hampus.farmangen@evidente.se>
2024-06-09 21:38:49 +0200

    Functions implemented

--- battleSips.c
+++ battleSips.c
@@ -8,8 +8,9 @@ const int dbg = 1;
 
 struct battleSea
 {
-    int player;
-    int **matrix;
+    int player;      // Player number
+    int **matrix;    // Sea
+    int *shipStatus; // Status of ships
 };
 
 // Allocate memory for sea
@@ -18,10 +19,21 @@ int initBattleSea(struct battleSea *sea)
     if (dbg)
         printf("- DBG: Initializing battleSea for Player %d...\n", (*sea).player);
 
+    // Allocate memory for shipStatus
+    (*sea).shipStatus = malloc(numberOfShips * sizeof(int *));
+
+    // Reset shipStatus values
+    for (int i = 0; i < seaDimY; i++)
+    {
+        (*sea).shipStatus[i] = 0;
+    }
+
+    // Allocate memory for sea
     (*sea).matrix = malloc(seaDimY * sizeof(int *));
     for (int i = 0; i < seaDimX; i++)
         (*sea).matrix[i] = malloc(seaDimX * sizeof(int));
 
+    // Reset sea values
     for (int y = 0; y < seaDimY; y++)
     {
         for (int x = 0; x < seaDimX; x++)
@@ -89,65 +101,51 @@ int placeShip(int ship, int orgX, int orgY, int orintation, struct battleSea *se
     return 1;
 }
 
-// Shoot at x,y
-// returns 1 on hit, zero on miss
+// Shoot at x,y, and update matrix and ShipStatus
+// Returns hit matrix value
 int shoot(int targX, int targY, struct battleSea *sea)
 {
-    if ((*sea).matrix[targX][targY] > 0 && (*sea).matrix[targX][targY] <= numberOfShips)
+    int hit = (*sea).matrix[targX][targY];
+    if (dbg)
+        printf("Shot: (%d, %d): %d ", targX, targY, hit);
+    if (hit > 0 && hit <= numberOfShips)
     {
-        if (dbg)
-            printf("- DBG: Hitting (%d, %d) = %d\n", targX, targY, (*sea).matrix[targX][targY]);
         (*sea).matrix[targX][targY] = (*sea).matrix[targX][targY] + numberOfShips;
+        (*sea).shipStatus[hit - 1]++;
         if (dbg)
-            printf("- DBG: Hit (%d, %d) = %d\n", targX, targY, (*sea).matrix[targX][targY]);
-        return 1;
+            printf("-> hit \n");
     }
     else
     {
-        if (dbg)
-            printf("- DBG: Missing (%d, %d) = %d\n", targX, targY, (*sea).matrix[targX][targY]);
         (*sea).matrix[targX][targY] = numberOfShips * 2 + 1;
         if (dbg)
-            printf("- DBG: Missed (%d, %d) = %d\n", targX, targY, (*sea).matrix[targX][targY]);
-        return 0;
+            printf("-> miss \n");
     }
+    return hit;
 }
 
-int checkSea(struct battleSea *sea)
+// Checks if hit sinks ship
+// Returns 1 if sink, else 0;
+int checkHit(int hit, struct battleSea *sea)
 {
-    if (dbg)
-        printf("Checking Status \n");
-
-    // Create array for shipcount
-    int seaStatus[numberOfShips];
-    for (int i = 0; i < numberOfShips; i++)
+    if ((hit > 0))
     {
-        seaStatus[i] = 0;
-    }
-
-    // Check how much is left of each chip
-    for (int x = 0; x < seaDimX; x++)
-    {
-        for (int y = 0; y < seaDimY; y++)
+        if (((*sea).shipStatus[hit - 1] == hit))
         {
-            if ((*sea).matrix[x][y] - 1 < numberOfShips)
-            {
-                seaStatus[(*sea).matrix[x][y] - 1]++;
-            }
             if (dbg)
-            {
-                for (int i = 0; i < numberOfShips; i++)
-                {
-                    printf("(%d, %d) \t seaStatus %d=%d\n", x, y, i, seaStatus[i]);
-                }
-            }
+                printf("SHIP %d SUNK!\n", hit);
+            return 1;
         }
     }
+    return 0;
+}
 
-    // Return 1 if there are ships left, else 0;
+// Return 1 if there are ships left, else 0;
+int checkShipArray(struct battleSea *sea)
+{
     for (int i = 0; i < numberOfShips; i++)
     {
-        if (seaStatus[i] != 0)
+        if ((*sea).shipStatus[i] < i+1)
         {
             return 1;
         }
@@ -177,18 +175,41 @@ int main(void)
         (printSea(seaP1));
 
     // Place ships for Player2
-    shoot(0, 0, &seaP1); // hit
+    checkHit(shoot(0, 0, &seaP1), &seaP1); // hit
     printSea(seaP1);
-    shoot(3, 3, &seaP1); // hit
+    checkHit(shoot(3, 3, &seaP1), &seaP1); // hit
     printSea(seaP1);
-    shoot(3, 0, &seaP1); // hit
+    checkHit(shoot(3, 0, &seaP1), &seaP1); // hit
+    printSea(seaP1);
+    checkHit(shoot(1, 0, &seaP1), &seaP1); // hit
+    printSea(seaP1);
+    checkHit(shoot(3, 2, &seaP1), &seaP1); // hit
+    printSea(seaP1);
+    checkHit(shoot(3, 1, &seaP1), &seaP1); // hit
     printSea(seaP1);
     // Shoot Player 1
     // Shoot Player 2
 
     // check sea
-    if (!checkSea(&seaP1)){
-        printf("Game finished!");
+    if (!checkShipArray(&seaP1))
+    {
+        printf("Game Over! \n");
+    }
+    else
+    {
+        printf("Game continues...\n");
+    }
+
+    checkHit(shoot(0, 3, &seaP1), &seaP1); // hit
+    printSea(seaP1);
+    // check sea
+    if (!checkShipArray(&seaP1))
+    {
+        printf("Game Over! \n");
+    }
+    else
+    {
+        printf("Game continues...\n");
     }
 
     return 0;
